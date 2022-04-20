845fc435cc6b7e535cecd958d2fbe65ae5ec8cf6
Steven <thai2@umbc.edu>
2022-04-19 22:37:41 -0400

    querying just for grocery item names as strings, creating basic index page

--- app.py
+++ app.py
@@ -12,15 +12,17 @@ app.secret_key = os.environ.get('SECRET_KEY')
 @app.route("/")
 def index():
     data = get_db()
-    return str(data)
+    return render_template("index.html", all_data=data)
 
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
         db = g._database = sqlite3.connect('grocery_list.db')
         cursor = db.cursor()
-        cursor.execute("select * from groceries")
-    return cursor.fetchall()
+        cursor.execute("select name from groceries")
+        all_data = cursor.fetchall()
+        all_data = [str(val[0]) for val in all_data]
+    return all_data
 
 @app.teardown_appcontext
 def close_connection(exception):
